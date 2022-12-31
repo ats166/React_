875784d38ae03f2fb9ebcc0de5153b21b3103e00
ats166 <45484403+ats166@users.noreply.github.com>
2022-12-31 18:33:23 +0900

    React_

--- README.md
+++ README.md
@@ -68,3 +68,23 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+### input EventHandler
+
+메소드| 실행 조건
+---|---
+onFocus|요소가 포커스를 받은 경우 실행
+onBlur | 요소가 포커스를 잃은 경우 실행
+onSelect | 일부 텍스트가 선택되면 실행
+onChange | 스크립트의 내용이 변경 될 때 실행
+onClick | 클릭을 하면 실행
+onDoubleClick | 더블 클릭을 하면 실행
+onMouseDown | 마우스 버튼을 눌렀을 때 실행
+onMouseUp | 마우스 버튼을 떼었을 때 실행
+onMouseOver | 요소에 마우스가 올라갔을 때 실행
+onMouserMove | 요소안에서 마우스가 움직일 때 실행
+onMouseOut | 요소에서 마우스가 벗어났을 때 실행
+onKeyDown | 키보드를 누를 시 실행
+onKeyUp | 키보드에서 손을 떼었을 때 실행
+onKeyPress | 입력을 받았을 때 실행 ( 영문입력 시 정상적으로 동작하지만 한글입력 시 동작 하지않음)
--- src/App.js
+++ src/App.js
@@ -9,12 +9,18 @@ function App(){
   // let [바뀐제목, 제목안변경] = useState(['여자 코트 추천', '강남 우동맛집', '파이썬 독학']);
   let [글제목, 제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬 독학']);
   let [좋아요, 좋아요추가] = useState([0,0,0])
+  let [날짜, 날짜변경] = useState(['2022-2-17', '2022-2-17', '2022-2-17'])
   let [modal, setModal] = useState(false);
+  let [모달제목, 모달제목변경] = useState('')
+  let [모달날짜, 모달날짜변경] = useState('')
+  let [InputData, setInputData] = useState('')
+  let [InputDate, setInputDate] = useState('')
   
   return (
     <div className="App">
       <div className="black-nav">
         <div> React Logo </div>
+        
       </div>
 
       {/* <button onClick={ () => {
@@ -27,19 +33,59 @@ function App(){
         글제목.map(function(name, i) {
           return (
           <div className="list" key={i}>
-            <h4 onClick={ () => { setModal(!modal)} } >{ name } </h4>
+            <h4 onClick={ () => { 
+              setModal(!modal)
+              let modaltitle = [...모달제목];
+              let modaldate = [...모달날짜];
+              modaldate = 날짜[i];
+              modaltitle = name;
+              모달날짜변경(modaldate)
+              모달제목변경(modaltitle)              
+              } 
+            } >{ name } </h4>
             <button onClick={ () => { 
               let likecopy = [...좋아요];
               likecopy[i] += 1;
               좋아요추가(likecopy)
             }}
               >❤️</button> <span>{좋아요[i]}</span>
-            <p>2월 17일 발행</p>
+            <p>{날짜[i]}</p>
+            <button onClick={ () => {
+              let copy = [...글제목]
+              copy.splice(i,1)
+              제목변경(copy)
+            }}
+            
+            >글 삭제</button>
 
           </div> 
           )
         })
       }
+      <div>
+      <input onChange={ (e) => {
+        setInputData(e.target.value);
+      }}
+      ></input>
+      <input type='date' onChange={ (e) => {
+        setInputDate(e.target.value);
+      }}
+      
+      ></input>
+      <button onClick={ () => {
+        let copy = [...글제목];
+        let copy2 = [...좋아요];
+        let copy3 = [...날짜];
+        InputData == '' ? 제목변경(글제목) : 제목변경(copy);
+        InputData == '' ? 좋아요추가(좋아요) : 좋아요추가(copy2);
+        InputDate == '' ? 날짜변경(InputDate) : 날짜변경(copy3);
+        copy3.unshift(InputDate)
+        copy2.unshift(0)
+        copy.unshift(InputData)
+      }}
+      > 글 생성</button>
+      </div>
+      
 
       {/* <div className="list">
         <h4>{ 글제목[0] } 
@@ -57,8 +103,8 @@ function App(){
 
       {
         modal ? <Modal
-        글제목={글제목}
-        글제목변경={글제목변경}
+        모달제목={모달제목}
+        모달날짜={모달날짜}
         /> : null
       }
 
@@ -71,14 +117,16 @@ function App(){
 function Modal(props) {
   return (
     <div className="modal">
-      <h4> { props.글제목[0] }</h4>
-      <p>날짜</p>
+      <h4> { props.모달제목 }</h4>
+      <p> 날짜 : { props.모달날짜} </p>
       <p>상세내용</p>
-      <button
+      {/* <button
       onClick={() => {
-        props.글제목변경() =>
+      let changetitle = [...props.글제목];
+      changetitle[0] = '여자 코트 추천';
+      props.제목변경(changetitle)      
       }}
-      >글 수정</button>
+      >글 수정</button> */}
   </div>
   )
 }
